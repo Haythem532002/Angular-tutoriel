a1dc266b91b029e6223cfd09048dcd53df0bf57a
Haythem532002 <khiarih06@gmail.com>
2024-08-29 15:57:11 +0100

    add angular form

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -112,10 +112,12 @@ export class HousingService {
   getAllHousingLocation(): HousingLocation[] {
     return this.housingLocationList;
   }
-
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find(
       (housingLocation) => housingLocation.id === id
     );
+  }
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(firstName, lastName, email);
   }
 }
